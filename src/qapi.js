d860a8557ba85d8ea9cfc07d307e4c5f656e2143
ipfsweb <83792514+ipfsweb@users.noreply.github.com>
2021-06-22 09:12:57 +0300

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60d1785d1dbc47001fe4e64f",
+				"60d1785d1dbc47001fe4e651"
+			]
+		},
+		"60d1785d1dbc47001fe4e64f": {
+			"id": "60d1785d1dbc47001fe4e64f",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60d1785d1dbc47001fe4e651": {
+			"id": "60d1785d1dbc47001fe4e651",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {
+			"cursor": "auto"
+		},
+		"seo": {}
+	}
+}
